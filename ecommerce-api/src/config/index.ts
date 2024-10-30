4ed1e5fdc3faaa779d1281bc6287271020f126f5
Samuel Osei Adu <asamuel355@yahoo.com>
2024-10-30 10:09:39 +0000

    initialize paystack response ->paystack-service

--- ecommerce-api/src/config/index.ts
+++ ecommerce-api/src/config/index.ts
@@ -3,9 +3,11 @@ dotenv.config();
 
 interface Config {
   PAYSTACK_SECRET_KEY: string;
+  PAYSTACK_BASE_URL: string;
 }
 
 export const config: Config = {
   PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || '',
+  PAYSTACK_BASE_URL:process.env.PAYSTACK_BASE_URL || '',
   // Add other environment variables
 };
---
+++ ecommerce-api/src/services/paystack.service.ts
@@ -0,0 +1,38 @@
+import axios from 'axios'
+import { config } from '../config'
+import logger from '../config/logger'
+
+export class PaystackService{
+  private readonly baseUrl = config.PAYSTACK_BASE_URL;
+  private readonly secretKey = config.PAYSTACK_SECRET_KEY
+  
+  //Paystack Headers
+  private getHeaders(){
+    return{
+      Authorization: `Bearer ${this.secretKey}`,
+      'Content-Type': 'application/json'
+    }
+  }
+  
+  //Initialize transaction
+  async initializeTransaction(data: {
+    email: string,
+    amount: number;
+    callback_url: string;
+    metadata: any
+  }): Promise<any>{
+    try{
+      const response = await axios.post(
+        `${this.baseUrl}/transaction/initialize`,
+        data,
+        { headers: this.getHeaders() }
+      );
+      return response.data
+    }catch(error){
+      logger.error('Paystack initialize transaction error: ', error)
+      throw error;
+    }
+  }
+  
+  
+}
