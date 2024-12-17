b2c31d94bd54cf76abb16bef1dc445bef98c4785
Samuel Osei Adu <asamuel355@yahoo.com>
2024-12-17 14:54:21 +0000

    checkout update

--- app/src/app/(routes)/checkout/page.tsx
+++ app/src/app/(routes)/checkout/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import CartItem from "@/components/cart-item";
 import NewsLetter from "@/components/news-letter";
 import Footer from "@/components/shared/footer";
@@ -12,7 +12,6 @@ import Link from "next/link";
 import React from "react";
 
 const Cart = () => {
-
   const { items, getTotal } = useCart();
 
   return (
@@ -28,61 +27,73 @@ const Cart = () => {
           </nav>
         </div>
 
-        <h1 className="text-4xl font-bold mb-8">YOUR CART</h1>
+        {items.length > 0 ? (
+          <>
+            <h1 className="text-4xl font-bold mb-8">YOUR CART</h1>
 
-        <div className="grid gap-8 lg:grid-cols-3">
-          <div className="lg:col-span-2">
-            <div className="space-y-4 max-h-[48rem] overflow-y-scroll">
-              {items.map((item) => (
-                <CartItem
-                  id={item.id}
-                  image={item.image}
-                  title={item.title}
-                  size={item.size}
-                  color={item.color}
-                  price={item.price}
-                  quantity={item.quantity}
-                  dropdown={false}
-                />
-              ))}
-            </div>
-          </div>
-
-          <div>
-            <Card>
-              <CardContent className="p-6">
-                <h2 className="text-xl font-semibold mb-4">Order Summary</h2>
-                <div className="space-y-4">
-                  <div className="flex justify-between">
-                    <span>Subtotal</span>
-                    <span>${'subtotal'}</span>
-                  </div>
-                  <div className="flex justify-between text-destructive">
-                    <span>Discount (-20%)</span>
-                    <span>-${'discount'}</span>
-                  </div>
-                  <div className="flex justify-between">
-                    <span>Delivery Fee</span>
-                    <span>${'deliveryFee'}</span>
-                  </div>
-                  <Separator />
-                  <div className="flex justify-between font-semibold text-lg">
-                    <span>Total</span>
-                    <span>${getTotal()}</span>
-                  </div>
-                  <div className="flex gap-2">
-                    <Input placeholder="Add promo code" />
-                    <Button variant="outline">Apply</Button>
-                  </div>
-                  <Button className="w-full" size="lg">
-                    Proceed to payment
-                  </Button>
+            <div className="grid gap-8 lg:grid-cols-3">
+              <div className="lg:col-span-2">
+                <div className="space-y-4 max-h-[48rem] overflow-y-scroll">
+                  {items.map((item) => (
+                    <CartItem
+                      id={item.id}
+                      image={item.image}
+                      title={item.title}
+                      size={item.size}
+                      color={item.color}
+                      price={item.price}
+                      quantity={item.quantity}
+                      dropdown={false}
+                    />
+                  ))}
                 </div>
-              </CardContent>
-            </Card>
+              </div>
+
+              <div>
+                <Card>
+                  <CardContent className="p-6">
+                    <h2 className="text-xl font-semibold mb-4">
+                      Order Summary
+                    </h2>
+                    <div className="space-y-4">
+                      <div className="flex justify-between">
+                        <span>Subtotal</span>
+                        <span>${"subtotal"}</span>
+                      </div>
+                      <div className="flex justify-between text-destructive">
+                        <span>Discount (-20%)</span>
+                        <span>-${"discount"}</span>
+                      </div>
+                      <div className="flex justify-between">
+                        <span>Delivery Fee</span>
+                        <span>${"deliveryFee"}</span>
+                      </div>
+                      <Separator />
+                      <div className="flex justify-between font-semibold text-lg">
+                        <span>Total</span>
+                        <span>${getTotal()}</span>
+                      </div>
+                      <div className="flex gap-2">
+                        <Input placeholder="Add promo code" />
+                        <Button variant="outline">Apply</Button>
+                      </div>
+                      <Button className="w-full" size="lg">
+                        Proceed to payment
+                      </Button>
+                    </div>
+                  </CardContent>
+                </Card>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div className="flex items-center justify-center">
+            <h4 className="p-3 border rounded">
+              Your cart is empty. Visit <Link href={"/"}>Shop</Link> Add
+              Products to cart
+            </h4>
           </div>
-        </div>
-        <NewsLetter />
+        )}
       </div>
     </>
   );
